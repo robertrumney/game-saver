89d93d8143f9ab8b3c814513b75bc4116b91e498
Robert Rumney <robertrumney@gmail.com>
2023-05-17 09:34:27 +0200

    Create EnemyAI.cs

---
+++ Dependencies/EnemyAI.cs
@@ -0,0 +1,9 @@
+// Blank script for example purposes - will implement more later  
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour 
+{
+  ///...
+}
